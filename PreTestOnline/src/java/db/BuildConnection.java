6b0d106df3d85f031bb04b81fb0f1929c3ba2592
Repeach <wasana25436@gmail.com>
2019-11-12 22:09:12 +0700

    [ADD] Buildconnection

--- PreTestOnline/src/java/db/BuildConnection.java
+++ PreTestOnline/src/java/db/BuildConnection.java
@@ -5,10 +5,42 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author JJ
  */
 public class BuildConnection {
+    private static final String DRIVER = "org.apache.derby.jdbc.ClientDriver";
+    private static final String URL = "jdbc:derby://localhost:1527/sample";
+    private static final String USERNAME = "app";
+    private static final String PASSWORD = "123";
+    private static boolean isLoad = false;
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        if(!isLoad){
+            try {
+                Class.forName(DRIVER);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(BuildConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        isLoad = true;
+        }
+        try {
+            conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException ex) {
+            Logger.getLogger(BuildConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
     
 }
