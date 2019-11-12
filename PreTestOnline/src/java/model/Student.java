5e91dc4e59ce7f31893f0672cfd029131031ab9a
Repeach <wasana25436@gmail.com>
2019-11-12 22:08:10 +0700

    [UPDATE] Student class

--- PreTestOnline/src/java/model/Student.java
+++ PreTestOnline/src/java/model/Student.java
@@ -10,5 +10,43 @@ package model;
  * @author JJ
  */
 public class Student {
-    
+    private String password;
+    private String userName;
+    private int studentId;
+
+    public Student(String userName, String password, int studentId) {
+        this.userName = userName;
+        this.password = password;
+        this.studentId = studentId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStudentID(int studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "userName=" + userName + ", password=" + password + ", studentId=" + studentId + '}';
+    }
+
 }
