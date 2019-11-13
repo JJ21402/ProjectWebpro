a08934e32d9a58416df8e336bbb2e3e04042b0ce
Repeach <wasana25436@gmail.com>
2019-11-13 21:51:45 +0700

    [ADD] Usercontroller

--- PreTestOnline/src/java/Controller/UserController.java
+++ PreTestOnline/src/java/Controller/UserController.java
@@ -7,67 +7,71 @@ package Controller;
 
 import db.BuildConnection;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.Student;
+import model.User;
 
 /**
  *
- * @author JJ
+ * @author bud
  */
-public class StudentController {
-    private final static String FIND_BY_ID = "select * from student where studentid = ?";
-    private final static String FIND_BY_USERNAME = "select * from student where username = ?";
+public class UserController {
 
-    public Student findById(int id) {
-        Student student = null;
+    private final static String FIND_BY_ID = "select * from user_data where userid = ?";
+    private final static String FIND_BY_USERNAME = "select * from user_data where username = ?";
+   
+
+    public User findById(int id) {
+        User user = null;
         Connection conn = BuildConnection.getConnection();
         try {
             PreparedStatement pstm = conn.prepareStatement(FIND_BY_ID);
             pstm.setInt(1, id);
             ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
-                student = ResultSetToStudent(rs);
+                user = ResultSetToUser(rs);
             }
             rs.close();
             conn.close();
         } catch (SQLException ex) {
-            Logger.getLogger(StudentController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return student;
+        return user;
     }
 
-    public Student findByUsername(String username) {
-        Student student = null;
+    public User findByUsername(String username)  {
+        User user = null;    
         Connection conn = BuildConnection.getConnection();
         try {
             PreparedStatement pstm = conn.prepareStatement(FIND_BY_USERNAME);
             pstm.setString(1, username);
             ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
-                student = ResultSetToStudent(rs);
+                user = ResultSetToUser(rs);
             }
             rs.close();
             conn.close();
         } catch (SQLException ex) {
-            Logger.getLogger(StudentController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return student;
+        return user;
     }
 
-    static Student ResultSetToStudent(ResultSet rs) {
+    static User ResultSetToUser(ResultSet rs) {
         try {
-            Student student = new Student(rs.getString("username"), rs.getString("password"), rs.getInt("studentId"));
-            if (student.getStudentId() != 0) {
-                return student;
+
+            User user = new User(rs.getString("username"), rs.getString("password"), rs.getInt("userId"));
+            if (user.getUserId() != 0) {
+                return user;
             }
         } catch (SQLException ex) {
-            Logger.getLogger(StudentController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
+
 }
