a1286d865ff98c876a4d73045b763a47db02a245
Repeach <wasana25436@gmail.com>
2019-11-15 14:16:37 +0700

    [UPDATE] User controller parameter

--- PreTestOnline/src/java/Controller/UserController.java
+++ PreTestOnline/src/java/Controller/UserController.java
@@ -7,13 +7,11 @@ package Controller;
 
 import db.BuildConnection;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.User;
+
 
 /**
  *
@@ -21,58 +19,88 @@ import model.User;
  */
 public class UserController {
 
-    private final static String FIND_BY_ID = "select * from userdata where userid = ?";
-    private final static String FIND_BY_USERNAME = "select * from userdata where username = ?";
-   
+    private final String FIND_BY_ID = "SELECT * FROM userdata WHERE userId = ?";
+    private final String FIND_BY_USERNAME = "SELECT * FROM userdata WHERE USERNAME = ?";
+    private final String ADD_NEWUSER = "INSERT INTO userdata (userid,fname,lname,email,username,password)"
+            + "VALUES (?,?,?,?,?,?)";
+    private final String FIND_LASTUSERID = "SELECT MAX(userId) FROM userdata";
 
-    public User findById(int id) {
-        User user = null;
-        Connection conn = BuildConnection.getConnection()
-                ;
+    public int findLastIndexUser() {
+        int i = -1;
+        Connection conn = BuildConnection.getConnection();
         try {
-            PreparedStatement pstm = conn.prepareStatement(FIND_BY_ID);
-            pstm.setInt(1, id);
+            PreparedStatement pstm = conn.prepareStatement(FIND_LASTUSERID);
             ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
-                user = ResultSetToUser(rs);
+                i = rs.getInt("1");
             }
-            rs.close();
             conn.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException e) {
+            System.out.println("Error Occured");
         }
-        return user;
+        return i;
     }
 
-    public User findByUsername(String username)  {
-        User user = null;    
+    public User FindUserById(int userId) {
+        User b = null;
+        Connection con = BuildConnection.getConnection();
+        try {
+            PreparedStatement pstm = con.prepareStatement(FIND_BY_ID);
+            pstm.setInt(1, userId);
+            ResultSet rs = pstm.executeQuery();
+
+            if (rs.next()) {
+               b = new User(rs.getInt("userId"),rs.getString("fname"), rs.getString("lname"), rs.getString("email"),rs.getString("username"),rs.getString("password"));
+            }
+            //  while         
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
+
+    public User findByUsername(String username) {
+        User b = null;
         Connection conn = BuildConnection.getConnection();
         try {
             PreparedStatement pstm = conn.prepareStatement(FIND_BY_USERNAME);
             pstm.setString(1, username);
             ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
-                user = ResultSetToUser(rs);
+               b = new User(rs.getInt("userId"),rs.getString("fname"), rs.getString("lname"), rs.getString("email"),rs.getString("username"),rs.getString("password"));
             }
-            rs.close();
             conn.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException e) {
+            System.out.println("Error");
         }
-        return user;
+        return b;
     }
-
-    static User ResultSetToUser(ResultSet rs) {
-        try {
-
-            User user = new User(rs.getString("username"), rs.getString("password"), rs.getInt("userId"));
-            if (user.getUserId() != 0) {
-                return user;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+    public boolean addNewUser(User rb){
+        Connection conn = BuildConnection.getConnection();
+        try{
+            PreparedStatement pstm = conn.prepareStatement(ADD_NEWUSER);
+            int last = findLastIndexUser();
+            System.out.println(last);
+            pstm.setInt(1,rb.getUserId());
+            pstm.setString(2, rb.getFname());
+            pstm.setString(3, rb.getLname());
+            pstm.setString(4, rb.getEmail());
+            pstm.setString(5, rb.getUsername());
+            pstm.setString(6, rb.getPassword());
+            int rs = pstm.executeUpdate();
+            conn.close();
+            return true;
         }
-        return null;
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
     }
-
+    public static void main(String[] args) {
+        UserController r1 =new UserController();
+        
+        User rr=new User("asdsadsadpedaasd","asd","asdasd", "kuy", "jj");
+      
+       
+    }
 }
