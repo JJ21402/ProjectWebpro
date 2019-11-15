088f180cf902a85ca4d4bf39b4a4108513c984cd
Repeach <wasana25436@gmail.com>
2019-11-15 14:16:57 +0700

    [UPDATE] User model parameter

--- PreTestOnline/src/java/model/User.java
+++ PreTestOnline/src/java/model/User.java
@@ -5,52 +5,91 @@
  */
 package model;
 
+import Controller.UserController;
+
 /**
  *
  * @author bud
  */
 public class User {
-    private String password;
-    private String username;
+
     private int userId;
+    private String fname;
+    private String lname;
+    private String email;
+    private String username;
+    private String password;
 
-    public User(String userName,String password,  int userId) {
-        this.username = userName;
-        this.password = password;
+    public User(int userId, String fname, String lname, String email, String username, String password) {
         this.userId = userId;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
+    public User(String fname, String lname, String email, String username, String password) {
+        UserController r1 = new UserController();
+        userId = r1.findLastIndexUser()+1;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
     public int getUserId() {
         return userId;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getFname() {
+        return fname;
     }
 
-    public void setUsername(String userName) {
-        this.username = userName;
+    public String getLname() {
+        return lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
-    @Override
-    public String toString() {
-        return "User{" + "password=" + password + ", username=" + username + ", userId=" + userId + '}';
+    public void setFname(String fname) {
+        this.fname = fname;
     }
 
-    
-    
-    
-    
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterBean{" + "userId=" + userId + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", username=" + username + ", password=" + password + '}';
+    }
+
 }
