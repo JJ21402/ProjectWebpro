c7a1d59f5dfa8594cbdfa31bafb5df212899efb6
Siriyakorn Suasosit <barbiepx165cm#gmail.com>
2019-11-17 19:21:28 +0700

    [UPDATE] Edit searchservlet but cant find now

--- PreTestOnline/src/java/Controller/SearchController.java
+++ PreTestOnline/src/java/Controller/SearchController.java
@@ -1,30 +1,30 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package Controller;
-//
-//import db.BuildConnection;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import model.Search;
-//
-///**
-// *
-// * @author Nitro5
-// */
-//public class SearchController {
-////    
-////    private final static String FIND_BY_GRADEID = "SELECT * FROM APP.STUDENT s RIGHT JOIN APP.EQUIPMENT e ON s.STUDENTID=e.STUDENTID WHERE e.EQUIPMENTID=?";
-////    private final static String FIND_ALL = "SELECT * FROM APP.STUDENT s RIGHT JOIN APP.EQUIPMENT e ON s.STUDENTID=e.STUDENTID";
-////    
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import db.BuildConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Search;
+
+/**
+ *
+ * @author Nitro5
+ */
+public class SearchController {
+//    
+//    private final static String FIND_BY_GRADEID = "SELECT * FROM APP.STUDENT s RIGHT JOIN APP.EQUIPMENT e ON s.STUDENTID=e.STUDENTID WHERE e.EQUIPMENTID=?";
+//    private final static String FIND_ALL = "SELECT * FROM APP.STUDENT s RIGHT JOIN APP.EQUIPMENT e ON s.STUDENTID=e.STUDENTID";
+//    
+
 //    public Search findByEquipmentId(int id) {
 //        Search searches = null;
 //        Connection conn = BuildConnection.getConnection();
@@ -42,7 +42,7 @@
 //        }
 //        return searches;
 //    }
-//
+
 //    public ArrayList<Search> findEquipmentEntities() {
 //        ArrayList<Search> searches = new ArrayList<>();
 //        Connection conn = BuildConnection.getConnection();
@@ -67,8 +67,8 @@
 //        }
 //        return null;
 //    }
-//}
-//
-//
-//
-//
+}
+
+
+
+
